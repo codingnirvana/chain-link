dc4439bfaa6402431858852444141084f48722cd
Rajesh Muppalla <1348422+codingnirvana@users.noreply.github.com>
2024-10-25 22:43:38 +0530

    Initial commit for Chain Game

---
+++ games.js
@@ -0,0 +1,259 @@
+        const games = [
+            {
+                startWord: 'Coffee',
+                chains: {
+                    'Coffee': ['Date'],
+                    'Date': ['Night'],
+                    'Night': ['Time'],
+                    'Time': ['Machine', 'Management'],
+                    'Machine': ['Gun'],
+                    'Gun': ['Powder'],
+                    'Powder': ['House'],
+                    'House': ['Party'],
+                    'Party': ['Animal']
+                }
+            },
+            {
+                startWord: 'Lean',
+                chains: {
+                    'Lean': ['Back'],
+                    'Back': ['Corner', 'Country'],
+                    'Country': ['Music'],
+                    'Music': ['Festival'],
+                    'Festival': ['Food'],
+                    'Food': ['Truck']
+                }
+            },
+            {
+                startWord: 'Train',
+                chains: {
+                    'Train': ['Track'],
+                    'Track': ['Suit'],
+                    'Suit': ['Coat'],
+                    'Coat': ['Check'],
+                    'Check': ['Book'],
+                    'Book': ['Case']
+                }
+            },
+            {
+                startWord: 'Fire',
+                chains: {
+                    'Fire': ['Alarm'],
+                    'Alarm': ['Clock'],
+                    'Clock': ['Tower'],
+                    'Tower': ['Bridge'],
+                    'Bridge': ['Gap']
+                }
+            },
+            {
+                startWord: 'Water',
+                chains: {
+                    'Water': ['Fall'],
+                    'Fall': ['Season'],
+                    'Season': ['Ticket'],
+                    'Ticket': ['Master'],
+                    'Master': ['Key']
+                }
+            },
+            {
+                startWord: 'Light',
+                chains: {
+                    'Light': ['House'],
+                    'House': ['Keeper'],
+                    'Keeper': ['Key'],
+                    'Key': ['Chain'],
+                    'Chain': ['Reaction']
+                }
+            },
+            {
+                startWord: 'Paper',
+                chains: {
+                    'Paper': ['Cut'],
+                    'Cut': ['Out'],
+                    'Out': ['Side'],
+                    'Side': ['Walk'],
+                    'Walk': ['Way']
+                }
+            },
+            {
+                startWord: 'Green',
+                chains: {
+                    'Green': ['House'],
+                    'House': ['Plant'],
+                    'Plant': ['Pot'],
+                    'Pot': ['Luck'],
+                    'Luck': ['Charm']
+                }
+            },
+            {
+                startWord: 'Snow',
+                chains: {
+                    'Snow': ['Ball'],
+                    'Ball': ['Room'],
+                    'Room': ['Service'],
+                    'Service': ['Charge'],
+                    'Charge': ['Card']
+                }
+            },
+            {
+                startWord: 'River',
+                chains: {
+                    'River': ['Bank'],
+                    'Bank': ['Account'],
+                    'Account': ['Balance'],
+                    'Balance': ['Sheet'],
+                    'Sheet': ['Music']
+                }
+            },
+            {
+                startWord: 'Blue',
+                chains: {
+                    'Blue': ['Sky'],
+                    'Sky': ['Line'],
+                    'Line': ['Up'],
+                    'Up': ['Hill'],
+                    'Hill': ['Top']
+                }
+            },
+            {
+                startWord: 'Wind',
+                chains: {
+                    'Wind': ['Chime'],
+                    'Chime': ['Bell'],
+                    'Bell': ['Tower'],
+                    'Tower': ['Block'],
+                    'Block': ['Chain']
+                }
+            },
+            {
+                startWord: 'Fast',
+                chains: {
+                    'Fast': ['Track'],
+                    'Track': ['Record'],
+                    'Record': ['Player'],
+                    'Player': ['Coach'],
+                    'Coach': ['Bus']
+                }
+            },
+            {
+                startWord: 'Iron',
+                chains: {
+                    'Iron': ['Man'],
+                    'Man': ['Power'],
+                    'Power': ['Ranger'],
+                    'Ranger': ['Station'],
+                    'Station': ['Master']
+                }
+            },
+            {
+                startWord: 'Space',
+                chains: {
+                    'Space': ['Ship'],
+                    'Ship': ['Deck'],
+                    'Deck': ['Chair'],
+                    'Chair': ['Person'],
+                    'Person': ['Guard']
+                }
+            },
+            {
+                startWord: 'Gold',
+                chains: {
+                    'Gold': ['Mine'],
+                    'Mine': ['Craft'],
+                    'Craft': ['Beer'],
+                    'Beer': ['Bottle'],
+                    'Bottle': ['Cap']
+                }
+            },
+            {
+                startWord: 'Rock',
+                chains: {
+                    'Rock': ['Star'],
+                    'Star': ['Dust'],
+                    'Dust': ['Bunny'],
+                    'Bunny': ['Hop'],
+                    'Hop': ['Scotch']
+                }
+            },
+            {
+                startWord: 'Glass',
+                chains: {
+                    'Glass': ['Bottle'],
+                    'Bottle': ['Neck'],
+                    'Neck': ['Tie'],
+                    'Tie': ['Knot'],
+                    'Knot': ['Rope']
+                }
+            },
+            {
+                startWord: 'Sun',
+                chains: {
+                    'Sun': ['Shine'],
+                    'Shine': ['Bright'],
+                    'Bright': ['Light'],
+                    'Light': ['Bulb'],
+                    'Bulb': ['Plant']
+                }
+            },
+            {
+                startWord: 'Tree',
+                chains: {
+                    'Tree': ['House'],
+                    'House': ['Warming'],
+                    'Warming': ['Gift'],
+                    'Gift': ['Wrap'],
+                    'Wrap': ['Around']
+                }
+            },
+            {
+                startWord: 'Ocean',
+                chains: {
+                    'Ocean': ['Wave'],
+                    'Wave': ['Length'],
+                    'Length': ['Breadth'],
+                    'Breadth': ['Height'],
+                    'Height': ['Measure']
+                }
+            },
+            {
+                startWord: 'Sugar',
+                chains: {
+                    'Sugar': ['Cane'],
+                    'Cane': ['Sugar'],
+                    'Sugar': ['Rush'],
+                    'Rush': ['Hour'],
+                    'Hour': ['Glass']
+                }
+            },
+            {
+                startWord: 'Honey',
+                chains: {
+                    'Honey': ['Bee'],
+                    'Bee': ['Hive'],
+                    'Hive': ['Mind'],
+                    'Mind': ['Set'],
+                    'Set': ['Piece']
+                }
+            },
+            {
+                startWord: 'Night',
+                chains: {
+                    'Night': ['Owl'],
+                    'Owl': ['Hoot'],
+                    'Hoot': ['Nanny'],
+                    'Nanny': ['Goat'],
+                    'Goat': ['Milk']
+                }
+            },
+            {
+                startWord: 'Mirror',
+                chains: {
+                    'Mirror': ['Image'],
+                    'Image': ['Viewer'],
+                    'Viewer': ['Discretion'],
+                    'Discretion': ['Advised'],
+                    'Advised': ['Action']
+                }
+            }
+        ];
+
